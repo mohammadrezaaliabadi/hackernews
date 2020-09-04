0ccdf3c4ed34dd0ef4281304506a5d31ec5c7012
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2020-09-04 22:11:37 +0430

    upgrade fetch data with axios. closes #2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 // function App() {
 //   return (
@@ -130,6 +131,7 @@ const PARAM_HPP = "hitsPerPage=";
 const DEFAULT_HPP = "10";
 
 class App extends Component {
+  _isMounted = false;
   constructor(props) {
     super(props);
     this.state = {
@@ -151,12 +153,13 @@ class App extends Component {
   }
 
   fetchSearchTopStories(searchTerm, page = 0) {
-    fetch(
+    axios(
       `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`
     )
-      .then((response) => response.json())
-      .then((result) => this.setSearchTopStories(result))
-      .catch((error) => this.setState({ error }));
+      .then(
+        (result) => this._isMounted && this.setSearchTopStories(result.data)
+      )
+      .catch((error) => this._isMounted && this.setState({ error }));
   }
 
   onSearchSubmit(event) {
@@ -183,9 +186,13 @@ class App extends Component {
     this.setState({ searchTerm: event.target.value });
   }
   componentDidMount() {
+    this._isMounted = true;
     const { searchTerm } = this.state;
     this.setState({ searchKey: searchTerm });
     this.fetchSearchTopStories(searchTerm);
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
   }
   onDismiss(id) {
     const { searchKey, results } = this.state;
