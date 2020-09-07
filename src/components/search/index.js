218e788b7e3b70083e7b92b387a4facbd848ef4e
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2020-09-07 22:25:57 +0430

    add focus to search input closes #6

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,11 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
-const Search = ({ value, onChange, onSubmit, children }) => (
-  <form onSubmit={onSubmit}>
-    <input type="text" value={value} onChange={onChange} />
-    <button type="submit">{children}</button>
-  </form>
-);
+// const Search = ({ value, onChange, onSubmit, children }) => {
+//   let input;
+//   return (
+//     <form onSubmit={onSubmit}>
+//       <input
+//         type="text"
+//         value={value}
+//         onChange={onChange}
+//         ref={(node) => (input = node)}
+//       />
+//       <button type="submit">{children}</button>
+//     </form>
+//   );
+// };
+
+class Search extends React.Component {
+  componentDidMount() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+  render() {
+    const { value, onChange, onSubmit, children } = this.props;
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={(node) => (this.input = node)}
+        />
+        <button type="submit">{children}</button>
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
   value: PropTypes.string,
